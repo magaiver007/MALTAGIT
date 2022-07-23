f0e42efe5ca8e4a52bcff7b3dbbc41b03f454f2c
Magaiver007 <58440856+magaiver007@users.noreply.github.com>
2022-07-23 15:05:48 +0200

    Initial commit

---
+++ kindaREADME.txt
@@ -0,0 +1,104 @@
+PIGGY BANK FOR 2 TWINS THAT WILL BE ABLE TO BE OPENED AFTER 5 YEARS AND ONLY IF BOTH OF THEM AGREE TO OPEN IT 
+
+-> function that initiates the piggybank and allows the parents to add money to it (ponly one parent)
+-> show how much money does the piggybank have only if you are the parent 
+-> make the piggybank available only after 5 years (it started when the kids were 15)
+-> open the piggybank and distribute the money to the kids only if they both agree to it 
+
+
+//////////
+1 contract upon initialization: after 5 years of its creation it will create/enable the other contracts (this contract will be responsible for deposit and withdrawal)
+1 for kid one to accept (save the sender's address and compare it to what the parents have set if not the same the contract cannot be fulfilled)
+1 for kid 2 to accept (save the sender's address and compare it to what the parents have set if not the same the contract cannot be fulfilled)
+//////////
+
+
+
+
+
+store the amount of funds each user added and the users address 
+check if the user has put already funds and if so just up the index value without adding a new user index
+
+
+ 
+table with 3 rows example
+
+
+user.address|	user.index|		funds.amount|
+0x23984			1				123
+				2				123123
+0x53453			1				1235
+0x45364			1				2345						
+
+	
+
+struct Funds{
+	address user.address;
+	uint256 user.index;
+	uint256 funds.amount;
+}
+Funds[] public funds;
+
+
+//make a view function that searches if the address exists in the table 
+function addressKey(address msg.sender) public view returns(bool){
+	for (uint i = 0; i < address.length; i++
+		return true 
+	)
+
+
+}
+
+
+//if user address is the same with msg.sender make key true 
+
+
+
+
+
+if key is true get push user.index and add dunds.amount data
+if key is false push user.address and initialize user.index and add funds.amount
+
+
+
+
+
+
+
+
+
+retrieve elements from the array depenting on the user.address or user.id
+
+function getDataWithId(uint256 _id) public view returns {
+	return user[_id].[];
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ piggybank_code .sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+
+/* Smart contract that lets anyone deposit coins into the contract
+    Only 2 specific addresses can withdraw the coins and only if both
+    agree to do so 
+*/
+
+pragma solidity >=0.6.6 <0.9.0;
+
+
+contract piggybank{
+
+    //initialize agreement clauses
+	//upon initialization the agreemnts are false by default
+    bool agreement;
+    bool a;
+    bool b;
+    uint256 _timenow;
+    uint deploymentTime;
+    uint256 withdrawalDate;
+	
+	constructor(){
+        agreement = false;
+		a = false;
+		b = false;
+        deploymentTime = block.timestamp;
+    }
+	
+	//functions that with the input of the user become true 
+	//each function costs gas fees
+    //each function can become true after 5 years (the time that the kids must wait until thwy can withdraw the funds)
+    function userone()public {
+        require (block.timestamp >= deploymentTime + 157788000, "it's not the time yet");
+        a = true;
+    }
+
+    function usertwo()public {
+        require (block.timestamp >= deploymentTime + 157788000, "it's not the time yet");
+        b = true;
+    }
+
+    //make agreement true only if both children are ready  
+    function makeAgreementTrue()public{
+        require (a == true, "The first child is not ready yet");
+        require (b == true, "The second child is not ready yet");
+        agreement = true ;
+    }
+
+    //initialization of the contributors array
+	struct Contributors{
+		address contributor;
+		uint256 amount;
+	}
+	
+    Contributors[] public contributors;
+    
+    //map which connects the address with the amount deposited
+    mapping(address => uint256) public nameToAmount;
+
+    //connects the amount with the sender and adds sender address to the list 
+    function addFunds() public payable {
+       contributors.push(Contributors(msg.sender, msg.value)); 
+       nameToAmount[msg.sender] += msg.value;
+    }
+
+    
+      
+    /*
+    function withdrawFunds() public payable{
+
+
+    }
+    */
+    //////////////////////////////////////////////////
+    /////////////////DEBUGGING SECTION/////////////////
+    //////////////////////////////////////////////////
+    //output functions (made for debugging)
+    function outputa() public view returns(bool) {
+        return a;
+    }
+    function outputb() public view returns(bool) {
+        return b;
+    }
+    function outputagreement() public view returns(bool) {
+        return agreement;
+    }
+    function timeOfDeployment() public view returns (uint){
+        return deploymentTime;
+    }
+    //debugging function dont mind it 
+    function clear() public {
+        a = false;
+        b = false;
+        agreement = false;
+    }
+
+}
